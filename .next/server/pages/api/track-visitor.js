b3c7c65add6a1d6b9fb259014b841c43721e1959
praveen-raj <praveenstocks01@outlook.com>
2025-07-28 00:58:05 +0530

    Remove .env.local and .next from repository and apply .gitignore properly

--- .next/server/pages/api/track-visitor.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/api/track-visitor.js";
-const runtime = require("../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/[root-of-the-server]__047654c7._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/src/pages/api/track-visitor.ts [api] (ecmascript)\" } [api] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/src/pages/api/track-visitor.ts [api] (ecmascript)\" } [api] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
